f59fa8b7d03b2f4beb80fa8ea4f88a4377117637
tahsinaltabib <nabilarah33@gmail.com>
2025-08-11 14:31:13 +0600

    error

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,6 @@ import Banner from '../components/Banner'
 import HomePart from '../components/HomePart'
 import ProjectPart from '../components/Project'
 import Ourfeature from '../components/Ourfeature'
-import Ourfeature from '../components/Ourfeature'
 
 const Home = () => {
   return (
